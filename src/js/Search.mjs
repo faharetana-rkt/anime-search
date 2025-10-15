ff2ad6fc6e62ac1b6da98ac5ef34e6b8e3a5b331
faharetana-rkt <rakotomaharitra@gmail.com>
2025-10-15 22:48:51 +0300

    modified file path again for search.mjs

--- src/js/Search.mjs
+++ src/js/Search.mjs
@@ -8,7 +8,7 @@ function templateFunction(anime) {
         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/></svg>
     </button>
-    <a href="anime_pages/index.html?anime=${anime.mal_id}">
+    <a href="/anime_pages/index.html?anime=${anime.mal_id}">
     <img src="${anime.images.webp.large_image_url}" alt="${anime.title} image" loading="lazy">
     </a>
     <p class="score">⭐ ${anime.score}</p>
